e19ad3489da80762e1141f72cf275fa9e64e05e2
Ayush Bhardwaj <68005026+TheAyushB@users.noreply.github.com>
2024-10-15 21:40:52 +0530

    Time: 19 ms (40.65%), Space: 43.9 MB (89.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,56 @@
+// 2D DP Approach
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int n = coins.length;
+//         int minCoinsDp[][] = new int[n + 1][amount + 1];
+
+//         for(int i = 0; i <= n; i++) {
+//             for(int remAmount = 0; remAmount <= amount; remAmount++) {
+//                 if(remAmount == 0)
+//                     minCoinsDp[i][remAmount] = 0;
+//                 else if(i == 0)
+//                     minCoinsDp[i][remAmount] = amount + 1; // invalid
+//                 else {
+//                     int dTake = minCoinsDp[i - 1][remAmount];
+//                     int take = Integer.MAX_VALUE;
+//                     if(remAmount >= coins[i - 1])
+//                         take = 1 + minCoinsDp[i][remAmount - coins[i - 1]];
+//                     minCoinsDp[i][remAmount] = Math.min(take, dTake);
+//                 }
+                
+//             }
+//         }
+//         if(minCoinsDp[n][amount] > amount)
+//             minCoinsDp[n][amount] = -1;
+//         return minCoinsDp[n][amount];
+//     }
+// }
+
+// 1D DP Approach
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int minCoinsDp[][] = new int[n + 1][amount + 1];
+        int minCoinsDp[] = new int[amount + 1];
 
-        for(int i = 0; i <= n; i++) {
-            for(int remAmount = 0; remAmount <= amount; remAmount++) {
+        for(int i = 0; i <= n; i ++)
+            for(int remAmount = 0; remAmount <= amount; remAmount ++)
+            {
                 if(remAmount == 0)
-                    minCoinsDp[i][remAmount] = 0;
+                minCoinsDp[remAmount] = 0;
                 else if(i == 0)
-                    minCoinsDp[i][remAmount] = amount + 1; // invalid
-                else {
-                    int dTake = minCoinsDp[i - 1][remAmount];
+                    minCoinsDp[remAmount] = amount + 1; // invalid
+                else
+                {
+                    int dTake = minCoinsDp[remAmount];
                     int take = Integer.MAX_VALUE;
                     if(remAmount >= coins[i - 1])
-                        take = 1 + minCoinsDp[i][remAmount - coins[i - 1]];
-                    minCoinsDp[i][remAmount] = Math.min(take, dTake);
-                }
-                
+                        take = 1 + minCoinsDp[remAmount - coins[i - 1]];
+                    minCoinsDp[remAmount] = Math.min(take, dTake);
+                } 
             }
-        }
-        if(minCoinsDp[n][amount] > amount)
-            minCoinsDp[n][amount] = -1;
-        return minCoinsDp[n][amount];
+        
+        if(minCoinsDp[amount] > amount)
+            minCoinsDp[amount] = -1;
+        return minCoinsDp[amount];
     }
 }
