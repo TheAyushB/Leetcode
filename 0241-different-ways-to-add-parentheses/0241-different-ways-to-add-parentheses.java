0b2221921ec53cdb4d8d0216ebe603da4f290a86
Ayush Bhardwaj <68005026+TheAyushB@users.noreply.github.com>
2024-10-22 22:31:05 +0530

    Time: 1 ms (99.74%), Space: 41.5 MB (97.85%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,31 +1,36 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String expression) {
-        List<Integer> ans = new ArrayList<>();
+    Map<String, List<Integer>> map = new HashMap<>();
+        public List<Integer> diffWaysToCompute(String expression) {
+            if(map.get(expression) != null)
+                return map.get(expression);
+        
+            List<Integer> ans = new ArrayList<>();
 
-        if(!expression.contains("+") && !expression.contains("-") && !expression.contains("*")) {
-            ans.add(Integer.parseInt(expression));
+            if(!expression.contains("+") && !expression.contains("-") && !expression.contains("*")) {
+                ans.add(Integer.parseInt(expression));
+                return ans;
+            }
+
+            int len = expression.length();
+            for(int i = 0; i < len; i++) {
+                char ch = expression.charAt(i);
+                if(!Character.isDigit(ch)) { // operand 
+                    List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                    List<Integer> right = diffWaysToCompute(expression.substring(i + 1, len));
+                    for(int x : left)
+                        for(int y : right){
+                            switch(ch) {
+                                case '+': ans.add(x + y);
+                                            break;
+                                case '-': ans.add(x - y);
+                                            break;
+                                case '*': ans.add(x * y);
+                                            break;
+                            }
+                        }
+                }
+            }
+            map.put(expression, ans);
             return ans;
         }
-
-        int len = expression.length();
-        for(int i = 0; i < len; i++) {
-            char ch = expression.charAt(i);
-            if(!Character.isDigit(ch)) { // operand 
-                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i + 1, len));
-                for(int x : left)
-                    for(int y : right){
-                        switch(ch) {
-                            case '+': ans.add(x + y);
-                                        break;
-                            case '-': ans.add(x - y);
-                                        break;
-                            case '*': ans.add(x * y);
-                                        break;
-                        }
-                    }
-            }
-        }
-        return ans;
-    }
 }
