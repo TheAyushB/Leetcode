13049d69e44b6f2e4616f8cfa1d44e21de1ed819
Ayush Bhardwaj <68005026+TheAyushB@users.noreply.github.com>
2024-11-11 22:16:38 +0530

    Time: 0 ms (100%), Space: 41.8 MB (74.37%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -14,3 +14,19 @@ class Solution {
         return nums[low];
     }
 }
+
+/*
+ // search in a rotated sorted array - similar question on LC
+
+  1. array - 0 --- low - 1 
+  2.         low - n - 1
+
+  Same Question;;;(KINDA):
+  1. find min - nums[low]
+  2. find inflection - low
+  3. find the number of rotations - low
+  4. search -> find low, apply binary search on 1. 0 --- low-1
+                                                2. low --- n-1
+
+
+*/
