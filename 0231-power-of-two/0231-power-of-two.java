8b4a56939b21ea4c5a8ffefedb4e4661bf957279
Ayush Bhardwaj <68005026+TheAyushB@users.noreply.github.com>
2025-02-19 15:40:57 +0530

    Time: 0 ms (100%), Space: 40.9 MB (47.18%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n < 1) return false;
-        if((n & (n - 1)) == 0) return true;
+
+        if(n < 1) return false; 
+
+        if((n & (n - 1)) == 0) return true; // power of 2 number will have only one 1
+                                            // number one-less than 2^n is all 1s
+                                            // & it and get 0
         return false;
     }
 }
